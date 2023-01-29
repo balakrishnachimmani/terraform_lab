173193e09bf490fa5447f517b5fa0bc3384b5c9e
balakrishnachimmani <53811397+balakrishnachimmani@users.noreply.github.com>
2023-01-29 16:45:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,55 @@
-
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-west-2"
- 
+  region = "us-west-2"
 }
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
 
-  name = "single-instance"
 
-  ami                    = "ami-00874d747dde814fa"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
- 
-  vpc_security_group_ids = ["	sg-02dc6d10c1b47f88c"]
- 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "tf-example"
   }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0aa7d40eeae50c9a9" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+}
 }
