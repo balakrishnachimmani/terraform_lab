4f4ba6cacefa96648d2a0afe93938f0da734af25
balakrishnachimmani <53811397+balakrishnachimmani@users.noreply.github.com>
2023-01-29 16:59:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,12 @@
 
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-west-2"
+
+
+resource "aws_s3_bucket" "bucket" {
+bucket = "jmsth21-bkgtucket"
+acl = "private"
+tags = {
+Name = "My bucket"
+Environment = "Dev"
 }
-
-
-
-
-
-resource "aws_s3_bucket" "b" {
-  bucket = "myawsbucket12345jkgtd554555"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
-
-
-
